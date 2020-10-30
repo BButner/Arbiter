1e61758a8726a6bff72339f38975a738e596b040
BButner <beau.butner@siu.edu>
2020-10-30 11:09:00 -0500

    Ensure that HarmonyUserRepository implements CoroutineCrudRepository

--- arbiter-service/src/main/java/com/bbutner/arbiter/service/model/HarmonyUserRepository.kt
+++ arbiter-service/src/main/java/com/bbutner/arbiter/service/model/HarmonyUserRepository.kt
@@ -1,9 +1,10 @@
 package com.bbutner.arbiter.service.model
 
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface HarmonyUserRepository {
+interface HarmonyUserRepository: CoroutineCrudRepository<HarmonyUser, String> {
     suspend fun getById(id: String): HarmonyUser?
 
     suspend fun getByEmail(email: String): HarmonyUser?
